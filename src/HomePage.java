381f858d5615a4f658745eb21d4664f1e793f9e6
ritage <ritageashraf2005@gmail.com>
2025-10-23 14:33:22 +0300

    Implemented View Students GUI + backend connection with home label

--- src/HomePage.java
+++ src/HomePage.java
@@ -116,6 +116,8 @@ public class HomePage extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         //For View Student
+        viewStudents view = new viewStudents();
+        view.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
